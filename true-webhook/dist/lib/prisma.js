542fd5fd76b5f4beced46dd2ebea77a5ca4dd325
True Webhook <dev@local>
2026-01-22 06:53:28 +0700

    Fix Prisma client import path

--- true-webhook/dist/lib/prisma.js
+++ true-webhook/dist/lib/prisma.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-const prisma_1 = require("../generated/prisma");
+const client_1 = require("@prisma/client");
 let prisma;
 if (process.env.NODE_ENV === "production") {
-    exports.prisma = prisma = new prisma_1.PrismaClient();
+    exports.prisma = prisma = new client_1.PrismaClient();
 }
 else {
     if (!global.__prisma) {
-        global.__prisma = new prisma_1.PrismaClient();
+        global.__prisma = new client_1.PrismaClient();
     }
     exports.prisma = prisma = global.__prisma;
 }
