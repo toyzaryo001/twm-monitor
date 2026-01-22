6bb6d08b44caf4c44e7ce22f1112261371f90d2c
True Webhook <dev@local>
2026-01-22 09:15:06 +0700

    Remove build artifacts, let Railway build fresh

--- true-webhook/dist/lib/prisma.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-declare global {
-    var __prisma: PrismaClient | undefined;
-}
-declare let prisma: PrismaClient;
-export { prisma };
-//# sourceMappingURL=prisma.d.ts.map
